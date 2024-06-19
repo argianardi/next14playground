1cfafc36f3fc2f3d2f4675dc45318eca0b1616ef
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-19 22:25:11 +0700

    Feat: Add product page

---
+++ src/app/product/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Product = () => {
+  return (
+    <div className="space-x-2 m-10 text-white font-bold">
+      <Link href={'/product/1'} className="bg-slate-700 px-4 py-2 rounded-md">
+        Product 1
+      </Link>
+      <Link href={'/product/2'} className="bg-slate-700 px-4 py-2 rounded-md">
+        Product 2
+      </Link>
+    </div>
+  );
+};
+
+export default Product;
