737e78902e3ee338d5ba0f999ee79a2c2b4eeb4c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-20 14:15:16 +0700

    Fix: Fix app icon link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'], display: 'swap', adjustFontFallback: false})
 
 export const metadata: Metadata = {
   title: 'Next 14 Playground',
@@ -17,8 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.ico" sizes="any" />
         <link rel="icon" href="/icon.png" type="image/png" sizes="16x16" />
+        <link
+          rel="icon"
+          href="/apple-icon.png"
+          type="image/png"
+          sizes="16x16"
+        />
         <link
           rel="apple-touch-icon"
           href="/icon.png"
@@ -26,7 +34,32 @@ export default function RootLayout({
           sizes="16x16"
         />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/blog" prefetch={false}>
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link href="/about" prefetch={false}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Link href="/product">Product</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main>{children}</main>
+        <footer>[Footer]</footer>
+      </body>
     </html>
   );
 }
