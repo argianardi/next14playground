6569162c1a84235c4936d4321fd8641fdf83adb4
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-21 11:20:15 +0700

    Feat: Add post Card

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import React from 'react';
+
+const PostCard = ({
+  author,
+  date,
+  description,
+  href,
+  image,
+  title,
+}: {
+  author: string;
+  date: string;
+  description: string;
+  href: string;
+  image: string;
+  title: string;
+}) => {
+  return (
+    <div className="flex flex-wrap">
+      <div className="mb-4 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-2/12">
+        <div className="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg">
+          <img src={image} alt="" className="w-full" />
+          <Link href={href}>
+            <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out opacity-0 hover:opacity-100 bg-[hsla(0,0%,8%,0.5)]"></div>
+          </Link>
+        </div>
+      </div>
+
+      <div className="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-9/12 xl:w-7/12">
+        <h5 className="text-lg font-bold">{title}</h5>
+        <p className="mb-3 text-gray-800">
+          <small>
+            published <u>{date}</u> by <a href="#!">{author}</a>
+          </small>
+        </p>
+        <p className="text-gray-800 font-playfair">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
