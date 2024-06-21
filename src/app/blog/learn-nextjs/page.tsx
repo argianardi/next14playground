9d03ffa56821ccca6c7eb1e1a9bc57fa3f7c1044
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-21 16:51:39 +0700

    Refactor: Split data layer and UI layer

--- src/app/blog/learn-nextjs/page.tsx
+++ src/app/blog/learn-nextjs/page.tsx
@@ -1,37 +1,24 @@
 import Heading from '@/components/Heading';
-import { readFile } from 'fs/promises';
-import path from 'path';
-import { marked } from 'marked';
-import metter from 'gray-matter';
+import { getPost } from '@/libs/post';
 import React from 'react';
 
 const LearnNext = async () => {
-  const filePath = path.join(
-    process.cwd(),
-    'src/contents/blog/belajar-nextjs.md'
-  );
-  const text = await readFile(filePath, 'utf8');
-  const {
-    content,
-    data: { title, image, date, author },
-  } = metter(text);
-  const html = marked(content);
-
+  const post = await getPost('belajar-nextjs');
   return (
     <>
-      <Heading>{title}</Heading>
+      <Heading>{post.title}</Heading>
       <p className="italic text-sm pb-2">
-        {date} - {author}
+        {post.date} - {post.author}
       </p>
       <img
-        src={image}
+        src={post.image}
         alt="natural"
         width={640}
         height={360}
         className="mb-2 rounded"
       />
       <article
-        dangerouslySetInnerHTML={{ __html: html }}
+        dangerouslySetInnerHTML={{ __html: post.body }}
         className="prose max-w-screen-sm text-red-900"
       />
     </>
