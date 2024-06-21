2b450a93920d07545b9ccc57eec9483bba09fc4d
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-22 00:03:11 +0700

    Feat: Add page title

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,10 +1,16 @@
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Next 14 Playground on blog page',
+};
 
 const BlogLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex">
-      <div className="border border-black">Sidebar</div>
-      <div>{children}</div>;
+      <div className="">[Sidebar]</div>
+      <div className="px-4">{children}</div>
     </div>
   );
 };
