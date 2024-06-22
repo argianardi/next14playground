6720bb494fd1d0b98fa8c62b57c0d788f03ea380
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-22 23:38:58 +0700

    Fix: Add next Image

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -20,7 +21,13 @@ const PostCard = ({
     <div className="flex flex-wrap">
       <div className="mb-4 w-full shrink-0 grow-0 basis-auto md:mb-0 md:w-2/12">
         <div className="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg">
-          <img src={image} alt="" className="w-full" />
+          <Image
+            src={image}
+            width={500}
+            height={500}
+            alt="post"
+            className="w-full"
+          />
           <Link href={href}>
             <div className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed transition duration-300 ease-in-out opacity-0 hover:opacity-100 bg-[hsla(0,0%,8%,0.5)]"></div>
           </Link>
