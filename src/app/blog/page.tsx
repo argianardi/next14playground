4f4b0b4d482831be9fac0d01a8c0ff93c866af20
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-22 13:06:03 +0700

    Feat: Add post card component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,29 @@
+import Heading from '@/components/Heading';
+import PostCard from '@/components/PostCard';
 import React from 'react';
+import { inter } from '../fonts';
+import { getAllContents } from '@/libs/post';
 
-const Blog = () => {
-  return <div>Blog</div>;
+const BlogPage = async () => {
+  const contens = await getAllContents();
+
+  return (
+    <>
+      <Heading>Blog Page</Heading>
+      <h2 className={`text-2xl mb-3 ${inter.className}`}>List of Post</h2>
+      {contens?.map((content, index) => (
+        <PostCard
+          key={index}
+          author={content.author}
+          date={content.date}
+          description={content.description}
+          href={`/blog/${content.slug}`}
+          image={content.image}
+          title={content.title}
+        />
+      ))}
+    </>
+  );
 };
 
-export default Blog;
+export default BlogPage;
