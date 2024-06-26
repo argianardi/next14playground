829fc29b63c2c4ae2cdf67467a307523614ac921
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-06-26 09:20:40 +0700

    Feat: Add pagination feature

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,17 +3,36 @@ import PostCard from '@/components/PostCard';
 import React from 'react';
 import { inter } from '../fonts';
 import { getAllContents } from '@/libs/post';
+import Link from 'next/link';
+import Pagination from '@/components/Pagination';
 
 export const revalidate = 30;
 
-const BlogPage = async () => {
-  const contens = await getAllContents();
+const BlogPage = async (params: {
+  params: Record<string, string>;
+  searchParams: {
+    page?: string;
+  };
+}) => {
+  // console.log(params); // { params: {}, searchParams: { page: '3' } }
+
+  const page = parsePageParam(params.searchParams.page);
+
+  const { meta, contents } = await getAllContents(3, page);
+  console.log('post', meta);
 
   return (
     <>
       <Heading>Blog Page</Heading>
       <h2 className={`text-2xl mb-3 ${inter.className}`}>List of Post</h2>
-      {contens?.map((content, index) => (
+
+      <Pagination
+        href={'/blog'}
+        page={page}
+        pageCount={meta.pagination.pageCount}
+      />
+
+      {contents?.map((content, index: number) => (
         <PostCard
           key={index}
           author={content.author}
@@ -29,3 +48,14 @@ const BlogPage = async () => {
 };
 
 export default BlogPage;
+
+const parsePageParam = (paramValue: string | undefined) => {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
+};
