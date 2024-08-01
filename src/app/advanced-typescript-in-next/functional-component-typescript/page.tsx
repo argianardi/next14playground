d509d2271b5e50896baa4f207221e3e9292eb82a
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-01 14:41:40 +0700

    Feat: Add Book Page

---
+++ src/app/advanced-typescript-in-next/functional-component-typescript/page.tsx
@@ -0,0 +1,11 @@
+import Book from '@/components/advanced_typescript/functional_component_typescript/Book';
+
+const BookPage = () => {
+  return (
+    <div>
+      <Book title="Bulan memeluk Bumi" author="Usop" />
+    </div>
+  );
+};
+
+export default BookPage;
