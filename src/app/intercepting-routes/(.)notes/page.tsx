743b6e48cf70a651c044714727365bdef6829355
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-08 22:25:04 +0700

    Feat: Add intercepting notes

---
+++ src/app/intercepting-routes/(.)notes/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotesIntercepting = () => {
+  return <div className="text-green-700 font-bold">Notes Intercepting</div>;
+};
+
+export default NotesIntercepting;
