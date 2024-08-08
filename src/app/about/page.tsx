29815192e3dc794770db3fc25705022b7e7f8816
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-08 22:24:30 +0700

    Feat: Add a link to go to notes page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import Link from 'next/link';
 
 const About = () => {
-  return <div className="h-[1000px]">About Page</div>;
+  return (
+    <div className="h-[1000px]">
+      <h1>About Page</h1>
+      <Link href={'/intercepting-routes/notes'} className="mt-5">
+        Go to Notes
+      </Link>
+    </div>
+  );
 };
 
 export default About;
