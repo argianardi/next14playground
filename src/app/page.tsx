e191ee9cef6904986abd50b6c872a04b8495dea9
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-08 10:05:50 +0700

    Fix: Fix home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Heading from '@/components/Heading';
 
 export default function Home() {
-  return <Heading>Welcom to home</Heading>;
+  return (
+    <>
+      <Heading>Welcom to home</Heading>
+      <div className="h-[1000px]">Home Page</div>
+    </>
+  );
 }
