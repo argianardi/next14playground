52967b0c4723fca9062434a83167d2840c29b37c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-08 21:39:04 +0700

    Feat: Add user page

---
+++ src/app/dahsboard/@users/page.tsx
@@ -0,0 +1,8 @@
+import DashboardCard from '@/components/DashboardCard';
+import React from 'react';
+
+const Users = () => {
+  return <DashboardCard>Users</DashboardCard>;
+};
+
+export default Users;
