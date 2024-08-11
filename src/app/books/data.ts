69b20f7e85b89dbd0f75001d4cff5d9fbdfaf12c
Argian Ardi Prasetya <argianardi1435@gmail.com>
2024-08-11 11:34:32 +0700

    Feat: Add book data

---
+++ src/app/books/data.ts
@@ -0,0 +1,44 @@
+export const books = [
+  {
+    id: 1,
+    title: 'To Kill a Mockingbird',
+    userId: 1,
+    description:
+      'A classic novel about racial injustice and the loss of innocence in a small Alabama town during the 1930s.',
+  },
+  {
+    id: 2,
+    title: 'The Great Gatsby',
+    userId: 2,
+    description:
+      'A novel set in the 1920s about the American Dream and the corrupting influence of wealth.',
+  },
+  {
+    id: 3,
+    title: 'Pride and Prejudice',
+    userId: 3,
+    description:
+      'A romantic novel about the lives of the Bennett sisters and their relationships with the wealthy Mr. Darcy.',
+  },
+  {
+    id: 4,
+    title: 'The Catcher in the Rye',
+    userId: 1,
+    description:
+      "A coming-of-age novel about a teenager's struggles with identity and belonging in the 1950s.",
+  },
+  {
+    id: 5,
+    title: '1984',
+    userId: 2,
+    description:
+      'A dystopian novel about a totalitarian future society and the dangers of government control.',
+  },
+  {
+    id: 6,
+    title: 'The Lord of the Rings',
+    userId: 3,
+    description:
+      "A high fantasy novel about a hobbit's journey to destroy the One Ring and save Middle-earth from the dark lord Sauron.",
+  },
+];
